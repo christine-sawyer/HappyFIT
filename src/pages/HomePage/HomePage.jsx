fbe9d09711733d313bd9ad0a50b2c8a90fa57c2a
Christine Sawyer <ChristineSawyerDance@gmail.com>
2023-09-03 02:38:23 -0400

    Basic HomePage styling - Mobile

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,39 @@
 import './HomePage.scss';
 
+import logo from "../../assets/logos/logo-no-background.png";
+import penguin from "../../assets/images/sample-penguin.jpg";
+import { Link } from "react-router-dom";
+
 export const HomePage = () => {
   
   return (
     <main className = "homepage">
-        <h1>Welcome to HappyFIT!</h1>
+        <section className = "homepage__content">
+          <img className = "homepage__logo"
+                  src = {logo}
+                  alt = "HappyFIT logo"
+              />
+          <h1 className = "homepage__title">Welcome to HappyFIT!</h1>
 
-        <h3>Exercise Library</h3>
+        <div className = "homepage__link-container">
+          <Link to ="/instructions" className = "homepage__link">
+              Instructions
+          </Link>
 
-        <h3>Technique</h3>
-        <p>Use your webcam to get feedback on your form.</p>
+          <Link to ="/exercises" className = "homepage__link">
+              Exercise Library
+          </Link>
+
+          <Link to ="/about" className = "homepage__link">
+              About
+          </Link>
+        </div>
+
+        </section>
+        <img className = "homepage__penguin"
+                  src = {penguin}
+                  alt = "HappyFIT penguin mascot"
+              />
 
         
     </main>
