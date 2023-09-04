5b88e9b0684123719f8f785860b6c15688744585
Christine Sawyer <ChristineSawyerDance@gmail.com>
2023-09-04 07:59:29 -0400

    Style edit.

--- src/pages/Instructions/Instructions.jsx
+++ src/pages/Instructions/Instructions.jsx
@@ -10,32 +10,38 @@ export const Instructions = () => {
   
   return (
     <main className = "instructions">
+      <h1 className = "instructions__title">How to train with HappyFIT Coach Cam!</h1>
         <section className = "instructions__container">
-          <h1 className = "instructions__title">How to train with HappyFIT Coach Cam!</h1>
-          <div className = "instructions__card">
-            <p className = "instructions__card-text">Accept camera access and wait for the webcam to load.</p>
-            <img 
-            className = "instructions__img--camera-popup"
-            src = {cameraPopup}
-            alt = "Accept camera popup"
-          />
-          </div>
-          
-          <div className = "instructions__card">
-            <p className = "instructions__card-text">Make sure your full body is in frame.</p>
-            <img 
-              className = "instructions__img--man-out"
-              src = {manOutOfFrame}
-              alt = "Cartoon of a man jumping out of frame"
+          <div className = "instructions__card-container">
+            <div className = "instructions__card">
+              <p className = "instructions__card-text">Accept camera access and wait for the webcam to load.</p>
+              <img 
+              className = "instructions__img--camera-popup"
+              src = {cameraPopup}
+              alt = "Accept camera popup"
             />
-          </div>
-          <div className = "instructions__card">
-            <p className = "instructions__card-text">Perform same pose next to camera. Mirror the image.</p>
-            <img 
-            className = "instructions__img--squat"
-            src = {squatInFrame}
-            alt = "Cartoon of lady doing a squat in frame"
-          />
+            </div>
+            
+            <div className = "instructions__card">
+              <p className = "instructions__card-text">
+                Make sure your full body is in frame.
+              </p>
+              <img 
+                className = "instructions__img--man-out"
+                src = {manOutOfFrame}
+                alt = "Cartoon of a man jumping out of frame"
+              />
+            </div>
+            <div className = "instructions__card">
+              <p className = "instructions__card-text">
+                Perform same pose as next to camera. Mirror image.
+              </p>
+              <img 
+              className = "instructions__img--squat"
+              src = {squatInFrame}
+              alt = "Cartoon of lady doing a squat in frame"
+            />
+            </div>
           </div>
           <div className = "instructions__links-container">
             <Link to ="/" className = "instructions__link">
