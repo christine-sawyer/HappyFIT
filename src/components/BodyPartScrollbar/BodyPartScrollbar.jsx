554c1082f342a9a9620492e3e12f3b85df22089a
Christine Sawyer <ChristineSawyerDance@gmail.com>
2023-09-04 10:17:52 -0400

    Create fetchData, BodyPartScrollbar. Come back to this stuff later if time

---
+++ src/components/BodyPartScrollbar/BodyPartScrollbar.jsx
@@ -0,0 +1,9 @@
+
+
+export const BodyPartScrollbar = () => {
+    return (
+      <section className = "body-part-scrollbar">
+        <h1 className = "body-part-scrollbar__title">Exercise Library</h1>
+      </section>
+    )
+  }
