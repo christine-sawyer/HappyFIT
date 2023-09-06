e5ae0eb22a1c15db45d8a3eb6dc0eb1a32ee0178
Christine Sawyer <ChristineSawyerDance@gmail.com>
2023-09-06 09:57:46 -0400

    Style

--- src/components/PopularExercises/PopularExercises.jsx
+++ src/components/PopularExercises/PopularExercises.jsx
@@ -1,99 +1,90 @@
-import './PopularExercises.scss';
+import "./PopularExercises.scss";
 
-import { Link } from 'react-router-dom';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { Link } from "react-router-dom";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
-import squat from '../../assets/images/squatleft.png'
-import highPlank from '../../assets/images/HighPlank.png'
-import forearmPlank from '../../assets/images/forearmPlank.png'
-import lateralRaise from '../../assets/images/lateralRaise.png'
-
+import squat from "../../assets/images/squatleft.png";
+import highPlank from "../../assets/images/HighPlank.png";
+import forearmPlank from "../../assets/images/forearmPlank.png";
+import lateralRaise from "../../assets/images/lateralRaise.png";
 
 export const PopularExercises = () => {
-    return (
-      <section className = "popular-exercises">
-        <h1 className = "popular-exercises__title">Popular Exercises</h1>
-        <div className = "popular-exercises__card-container">
-            {/* <Link to ="/exercises/squat" style={{textDecoration: 'none'}}> */}
-            <AnchorLink href='#squat' style={{textDecoration: 'none'}}>
-            <div  className = "popular-exercises__card">
-              <p className = "popular-exercises__card-text-title">
-                Squat
+  return (
+    <section className="popular-exercises">
+      <h2 className="popular-exercises__title">Popular Exercises</h2>
+      <div className="popular-exercises__card-container">
+        {/* <Link to ="/exercises/squat" style={{textDecoration: 'none'}}> */}
+        <AnchorLink href="#squat" style={{ textDecoration: "none" }}>
+          <div className="popular-exercises__card">
+            <p className="popular-exercises__card-text-title">Squat</p>
+            <img
+              className="popular-exercises__img--squat"
+              src={squat}
+              alt="Cartoon of woman doing a squat"
+            />
+            <div className="popular-exercises__muscle-group-container">
+              <p className="popular-exercises__muscle-group popular-exercises__muscle-group--abs">
+                Abs
               </p>
-              <img 
-                className = "popular-exercises__img--squat"
-                src = {squat}
-                alt = "Cartoon of woman doing a squat"
-              />
-              <div className = "popular-exercises__muscle-group-container">
-                <p className = "popular-exercises__muscle-group popular-exercises__muscle-group--abs">
-                    Abs
-                </p>
-                <p className = "popular-exercises__muscle-group popular-exercises__muscle-group--butt-hips">
-                    Butt/Hips
-                </p>
-                <p className = "popular-exercises__muscle-group popular-exercises__muscle-group--calves-shins">
-                    Legs - Calves & Shins
-                </p>
-                <p className = "popular-exercises__muscle-group popular-exercises__muscle-group--thighs">
-                    Legs - Thighs
-                </p>
-              </div>
+              <p className="popular-exercises__muscle-group popular-exercises__muscle-group--butt-hips">
+                Butt/Hips
+              </p>
+              <p className="popular-exercises__muscle-group popular-exercises__muscle-group--calves-shins">
+                Legs - Calves & Shins
+              </p>
+              <p className="popular-exercises__muscle-group popular-exercises__muscle-group--thighs">
+                Legs - Thighs
+              </p>
             </div>
-            </AnchorLink>
-            {/* </Link>  */}
+          </div>
+        </AnchorLink>
+        {/* </Link>  */}
 
-            {/* <Link to ="/exercises/plank" style={{textDecoration: 'none'}}> */}
-            <AnchorLink href='#plank' style={{textDecoration: 'none'}}>
-            <div  className = "popular-exercises__card">
-              <p className = "popular-exercises__card-text-title">
-                Plank
+        {/* <Link to ="/exercises/plank" style={{textDecoration: 'none'}}> */}
+        <AnchorLink href="#plank" style={{ textDecoration: "none" }}>
+          <div className="popular-exercises__card">
+            <p className="popular-exercises__card-text-title">Plank</p>
+            <img
+              className="popular-exercises__img--forearm-plank"
+              src={forearmPlank}
+              alt="Cartoon of woman doing a forearm plank"
+            />
+            <img
+              className="popular-exercises__img--high-plank"
+              src={highPlank}
+              alt="Cartoon of man doing a high plank"
+            />
+            <div className="popular-exercises__muscle-group-container">
+              <p className="popular-exercises__muscle-group popular-exercises__muscle-group--abs">
+                Abs
               </p>
-              <img 
-                className = "popular-exercises__img--forearm-plank"
-                src = {forearmPlank}
-                alt = "Cartoon of woman doing a forearm plank"
-              />
-              <img 
-                className = "popular-exercises__img--high-plank"
-                src = {highPlank}
-                alt = "Cartoon of man doing a high plank"
-              />
-              <div className = "popular-exercises__muscle-group-container">
-                <p className = "popular-exercises__muscle-group popular-exercises__muscle-group--abs">
-                    Abs
-                </p>
-                <p className = "popular-exercises__muscle-group popular-exercises__muscle-group--back">
-                    Back
-                </p>
-              </div>
-              </div>
-            {/* </Link> */}
-            </AnchorLink>
+              <p className="popular-exercises__muscle-group popular-exercises__muscle-group--back">
+                Back
+              </p>
+            </div>
+          </div>
+          {/* </Link> */}
+        </AnchorLink>
 
-            <Link to ="/exercises/lateral-raise" style={{textDecoration: 'none'}}>
-            <div  className = "popular-exercises__card">
-              <p className = "popular-exercises__card-text-title">
-                Lateral Raise
+        <Link to="/exercises/lateral-raise" style={{ textDecoration: "none" }}>
+          <div className="popular-exercises__card">
+            <p className="popular-exercises__card-text-title">Lateral Raise</p>
+            <img
+              className="popular-exercises__img--lateral-raise"
+              src={lateralRaise}
+              alt="Cartoon of man doing a lateral raise"
+            />
+            <div className="popular-exercises__muscle-group-container">
+              <p className="popular-exercises__muscle-group popular-exercises__muscle-group--shoulders">
+                Shoulders
               </p>
-              <img 
-                className = "popular-exercises__img--lateral-raise"
-                src = {lateralRaise}
-                alt = "Cartoon of man doing a lateral raise"
-              />
-              <div className = "popular-exercises__muscle-group-container">
-                <p className = "popular-exercises__muscle-group popular-exercises__muscle-group--shoulders">
-                    Shoulders
-                </p>
-              </div>
-              </div>
-            </Link>         
-        </div>
-        <Link to ="/" className = "popular-exercises__link">
-            Home
+            </div>
+          </div>
         </Link>
-        
-
-      </section>
-    )
-  }
+      </div>
+      <Link to="/" className="popular-exercises__link">
+        🏠Home
+      </Link>
+    </section>
+  );
+};
--- src/components/SquatCamSidebar/SquatCamSidebar.jsx
+++ src/components/SquatCamSidebar/SquatCamSidebar.jsx
@@ -16,13 +16,13 @@ export const SquatCamSidebar = () => {
         {/* Add check marks next to items */}
         <section className = "squat-cam-sidebar__cue-container">
             <p className ="squat-cam-sidebar__cue-title">Checklist:</p>
-            <p className ="squat-cam-sidebar__cue">Feet shoulder-width apart.</p>
-            <p className ="squat-cam-sidebar__cue">Toes turned slightly outwards.</p>
-            <p className ="squat-cam-sidebar__cue">Weight in midfoot (avoid raising the toes or the heels).</p>
-            <p className ="squat-cam-sidebar__cue">Neutral spine throughout the squat (avoid arching or rounding back).</p>
-            <p className ="squat-cam-sidebar__cue">Keep your shoulders down and back.</p>
-            <p className ="squat-cam-sidebar__cue">Brace (activate) your core and abdominal muscles.</p>
-            <p className ="squat-cam-sidebar__cue">Try to keep thighs parallel to floor.</p>
+            <p className ="squat-cam-sidebar__cue">✔️ Feet shoulder-width apart.</p>
+            <p className ="squat-cam-sidebar__cue">✔️ Toes turned slightly outwards.</p>
+            <p className ="squat-cam-sidebar__cue">✔️ Weight in midfoot (avoid raising the toes or the heels).</p>
+            <p className ="squat-cam-sidebar__cue">✔️ Neutral spine throughout the squat (avoid arching or rounding back).</p>
+            <p className ="squat-cam-sidebar__cue">✔️ Keep your shoulders down and back.</p>
+            <p className ="squat-cam-sidebar__cue">✔️ Brace (activate) your core and abdominal muscles.</p>
+            <p className ="squat-cam-sidebar__cue">✔️ Try to keep thighs parallel to floor.</p>
         </section>
         </div>
 
